9bf25d3e4127bb8bb7bbd2a8ce6548f7b3b4f26b
cbae <xchris.bae@gmail.com>
2023-06-25 22:47:08 -0700

    working on user controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,10 +4,10 @@ const userController = {
     async getUser(req, res) {
         try {
             const userDataDB = await User.find()
-            .select('-__v')
+                .select('-__v')
 
             res.join(userDataDB);
-        }   catch (err) {
+        } catch (err) {
             console.log(err);
             res.status(500).json(err);
         }
@@ -16,18 +16,65 @@ const userController = {
     async getSingleUser(req, res) {
         try {
             const userDataDB = await User.findOne({ _id: req.params.userId })
-            .select('-__v')
-            .populate('friends')
-            .populate('thoughts');
+                .select('-__v')
+                .populate('friends')
+                .populate('thoughts');
 
-            if(!userDataDB) {
-                return res.status(404).json({ message: 'No user with this id! '});
+            if (!userDataDB) {
+                return res.status(404).json({ message: 'No user with this id! ' });
             }
 
             res.join(userDataDB);
-        }   catch (err) {
+        } catch (err) {
             console.log(err);
             res.status(500).json(err);
         }
     },
+
+    async createUser(req, res) {
+        try {
+            const userDataDB = await User.create(req.body)
+            res.json(userDataDB);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    async updateUser(req, res) {
+        try {
+            const userDataDB = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!userDataDB) {
+                res.status(404).json({ message: 'No user with this id!' });
+            }
+
+            res.json(userDataDB);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    async deleteUser(req, res) {
+        try {
+            const userDataDB = await User.findOneAndDelete({ _id: req.params.userId });
+
+            if (!userDataDB) {
+                res.status(404).json({ message: 'No user with this id!' });
+            }
+
+            await Thought.deleteMany({ _id: { $in: userDataDB.thoughts } });
+            res.json({ message: 'User and Thoughts deleted!' });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    
 }
